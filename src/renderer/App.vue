806a66eb040eff954473cf85f7e7fe8a77204798
CRYDEsigN <Cryden_da@mail.ru>
2018-02-21 16:01:26 +0300

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-navigation-drawer app permanent>
+            <v-toolbar flat>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-title class="title">
+                    Application
+                  </v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list dense class="pt-0">
+                <v-list-tile v-for="item in menu_items" :key="item.title" @click="">
+                  <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app></v-toolbar>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+      title: 'edu.pro client',
+      menu_items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ],
+    })
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
+  /* Global CSS */
+</style>
